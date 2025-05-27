20f4fc68fc7a7d621a0d6d22fd60dcc113a083ea
tienhung504 <nguyenthung504@gmail.com>
2025-05-27 23:07:22 +1000

    Update 2002.sql

--- 2002.sql
+++ 2002.sql
@@ -3,3 +3,9 @@ GROUP BY user_id
 HAVING 
 SUM(CASE WHEN type = 'Refinance' THEN 1 ELSE 0 END) >= 1
 AND SUM(CASE WHEN type = 'InSchool' THEN 1 ELSE 0 END) >= 1
+
+-- SELECT DISTINCT user_id FROM loans
+-- WHERE type = 'Refinance'
+-- INTERSECT
+-- SELECT DISTINCT user_id FROM loans
+-- WHERE type = 'InSchool'
