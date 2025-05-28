c37960c4165db316bd7c38a338bbd07cdcd74356
tienhung504 <nguyenthung504@gmail.com>
2025-05-29 00:05:40 +1000

    Update 2014.sql

--- 2014.sql
+++ 2014.sql
@@ -1,5 +1,12 @@
-select 
-    HOUR(order_timestamp_utc) as peak_hour, 
-    RANK() OVER (ORDER BY (COUNT(*) / COUNT(DISTINCT DATE_FORMAT(order_timestamp_utc, '%y-%m-%d'))) DESC) AS rnk
-from postmates_orders
-GROUP BY peak_hour
+WITH hourly_rank AS (
+    SELECT 
+        HOUR(order_timestamp_utc) AS peak_hour, 
+        (COUNT(*) / COUNT(DISTINCT DATE_FORMAT(order_timestamp_utc, '%y-%m-%d'))) AS average_order_volume,
+        RANK() OVER (ORDER BY (COUNT(*) / COUNT(DISTINCT DATE_FORMAT(order_timestamp_utc, '%y-%m-%d'))) DESC) AS rnk
+    FROM postmates_orders
+    GROUP BY peak_hour
+)
+SELECT peak_hour, average_order_volume
+FROM hourly_rank
+WHERE rnk = 1
+ORDER BY average_order_volume DESC, peak_hour ASC;
