56f9ec0bf8a1336a30808f80fc6a71b6474a4e66
tienhung504 <nguyenthung504@gmail.com>
2025-05-29 00:04:53 +1000

    Update 2014.sql

--- 2014.sql
+++ 2014.sql
@@ -1,6 +1,5 @@
 select 
     HOUR(order_timestamp_utc) as peak_hour, 
-    (COUNT(*) / COUNT(DISTINCT DATE_FORMAT(order_timestamp_utc, '%y-%m-%d'))) AS average_order_volume
+    RANK() OVER (ORDER BY (COUNT(*) / COUNT(DISTINCT DATE_FORMAT(order_timestamp_utc, '%y-%m-%d'))) DESC) AS rnk
 from postmates_orders
 GROUP BY peak_hour
-ORDER BY average_order_volume DESC
