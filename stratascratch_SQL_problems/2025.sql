a1c0743a1feb3e21c5c770a8f019a07c2827dc7b
tienhung504 <nguyenthung504@gmail.com>
2025-05-30 18:52:02 +1000

    Create 2025.sql

---
+++ stratascratch_SQL_problems/2025.sql
@@ -0,0 +1,8 @@
+WITH loyal_clients AS (
+    select user_id  from fact_events
+    GROUP BY user_id
+    HAVING COUNT(DISTINCT client_id) = 1)
+    
+SELECT client_id, COUNT(DISTINCT user_id) AS loyal_customer_number
+FROM fact_events 
+WHERE user_id IN (SELECT user_id FROM loyal_clients)
