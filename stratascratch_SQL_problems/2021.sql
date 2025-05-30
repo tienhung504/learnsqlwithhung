56bc3634fbfc6d10f74b54d788fe178fb93ccd22
tienhung504 <nguyenthung504@gmail.com>
2025-05-30 17:59:37 +1000

    Create 2021.sql

---
+++ stratascratch_SQL_problems/2021.sql
@@ -0,0 +1,7 @@
+WITH initial_calls AS (
+    SELECT request_id, call_duration,
+    ROW_NUMBER() OVER (PARTITION BY request_id ORDER BY created_on) AS rn
+    FROM redfin_call_tracking)
+    
+SELECT AVG(call_duration) FROM initial_calls
+WHERE rn = 1
